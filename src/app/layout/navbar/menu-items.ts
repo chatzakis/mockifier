7961ec6d30bfead4b0856b43088bf92d2b9f9e9a
Chatzakis Michael <chatzakisdev@gmail.com>
2025-05-18 20:09:10 +0300

    Source link

--- src/app/layout/navbar/menu-items.ts
+++ src/app/layout/navbar/menu-items.ts
@@ -2,6 +2,7 @@ export interface MenuItem{
     text: string,
     icon: string,
     path: string,
+    external?: boolean
 }
 
 export const menuItems = [
@@ -10,14 +11,20 @@ export const menuItems = [
         icon: 'home',
         path: '',
     },
-        {
+    {
         text: 'Excel to SQL',
         icon: 'grid_on',
         path: '/xlsx-to-sql',
     },
-        {
+    {
         text: 'Instructions',
         icon: 'info',
         path: '/instructions',
+    },
+    {
+        text: 'Source',
+        icon: 'code',
+        path: 'https://github.com/chatzakis/mockifier',
+        external: true
     },
 ]
