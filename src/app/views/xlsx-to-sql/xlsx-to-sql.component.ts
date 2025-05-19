5cc54589127759324be38019f55d1e31e5e961dd
Chatzakis Michael <chatzakisdev@gmail.com>
2025-05-19 23:19:29 +0300

    Minor form fixes

--- src/app/views/xlsx-to-sql/xlsx-to-sql.component.ts
+++ src/app/views/xlsx-to-sql/xlsx-to-sql.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild, viewChild } from '@angular/core';
 import { FileInputComponent } from "../../shared/file-input/file-input.component";
 import { ExcelData } from '../../models/models';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
 import { exportSQLfromExcel } from '../../utilities/file-export';
 
 @Component({
@@ -10,7 +10,7 @@ import { exportSQLfromExcel } from '../../utilities/file-export';
   templateUrl: './xlsx-to-sql.component.html',
   styleUrl: './xlsx-to-sql.component.scss'
 })
-export class XlsxToSqlComponent {
+export class XlsxToSqlComponent implements AfterViewInit {
 
   excelData: ExcelData;
 
@@ -26,12 +26,22 @@ export class XlsxToSqlComponent {
   }
   //#endregion
 
-  tableName: string;
-  filename = 'excel-queries';
+  @ViewChild('sqlExportForm') sqlExportForm: NgForm;
+
+  ngAfterViewInit(){
+     setTimeout(() => {
+      if (this.sqlExportForm.controls['filename']) {
+        this.sqlExportForm.controls['filename'].setValue('excel-queries');
+      }
+    });
+  }
+  
   isValid = (name: string) => /^(?!^(CON|PRN|AUX|NUL|COM[1-9]|LPT[1-9])(\..*)?$)[^<>:"/\\|?*\x00-\x1F]+[^<>:"/\\|?*\x00-\x1F .]$/.test(name);
 
-  initSqlExport(){
-    exportSQLfromExcel(this.excelData, this.tableName, this.filename);
+  initSqlExport(formData: NgForm){
+    if(formData.form.valid){
+      exportSQLfromExcel(this.excelData, formData.value.tableName, formData.value.filename);
+    }
   }
 
 }
